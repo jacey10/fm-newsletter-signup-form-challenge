4f417f017462876aceb815d43144ff4bc60b92d6
Jacey <jaceyopara@gmail.com>
2026-01-28 08:48:23 +0100

    added classes for retrieval from DOM

---
+++ script.js
@@ -0,0 +1,9 @@
+const signupBlock = document.querySelector('.main__signup--block');
+const form = document.querySelector('.main__signup--form');
+const email = document.querySelector('.email');
+const emailError = document.querySelector('.email--error');
+const successBlock = document.querySelector('.main__success--block');
+const successEmail = document.querySelector('success--email');
+const dismissBtn = document.querySelector('.dismiss--button');
+
+
